606edf59d20674ed78ac9c6563bdbee26845d238
Sree Lakshmi <sinisreelaksmi19@gmail.com>
2025-04-14 14:26:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Toggle mobile menu
-
-const tog = document.getElementById("menu-toggle");
-const menu = document.getElementById("mobile-menu");
-
-tog.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
